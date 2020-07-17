0029c0969e1fa07eb773ca0b6965303574bb35ed
zengshuai <49233638+zengchuanqi@users.noreply.github.com>
2020-07-17 21:43:17 +0800

    Update sp.py

--- again/sp.py
+++ again/sp.py
@@ -3,13 +3,7 @@ import time
 from selenium import webdriver
 
 
-
-
-while True:
-    time
-    times = time.strftime('%H_%M', time.localtime(time.time()))
-    if times =='22_52':
-        wd=webdriver.Chrome()
-        wd.get('https://baidu.com')
-        break
-    time.sleep(58)
+wd=webdriver.Chrome()
+wd.get('https://www.baidu.com')
+print('ok')
+wd.quit()
